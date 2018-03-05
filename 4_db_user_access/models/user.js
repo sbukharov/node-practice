ff314eb36682787539a431fe12b23969bdbe4061
sbukharov <sergey.bukharov@cluberudite.com>
2018-03-05 11:47:40 -0800

    Added passport.js login authentication.

--- 4_db_user_access/models/user.js
+++ 4_db_user_access/models/user.js
@@ -27,12 +27,49 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
+/** 
+  External function allowing to search for users by id.
+  @param {number} id the id of the user
+*/
+module.exports.getUserById = function(id, callback) {
+  User.findById(id,callback);
+}
+
+/** 
+  External function allowing to search for users by username.
+  @param {number} id the username of the user
+*/
+module.exports.getUserByUsername = function(username, callback) {
+  var query = {username: username};
+  User.findOne(query, callback);
+}
+
+/** 
+  External function allowing to compare candidate password to user password.
+  @param {string} candidateFunction the candidate hash
+  @param {string} hash the password hash to compare to the candidate passwordpassword
+*/
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+  // Load hash from your password DB.
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+      callback(null, isMatch);
+  });
+}
+
+/** 
+  External function allowing to create user by passing in new User.
+  @param newUser, new user to create, should include:
+    username (String)
+    password (String)
+    email (String)
+    name (String)
+    profileimage (String) (optional)
+*/
 module.exports.createUser = function(newUser, callback) {
     bcrypt.genSalt(10, function(err, salt) {
       bcrypt.hash(newUser.password, salt, function(err, hash) {
         newUser.password = hash; 
         newUser.save(callback);
       });
-});
-  
+    });  
 }
