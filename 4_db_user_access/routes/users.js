e8fec8d7f4ea62e6c5604a5c23915797a03d7a6b
sbukharov <sergey.bukharov@cluberudite.com>
2018-03-05 10:58:16 -0800

    Created form validation for registration form.

--- 4_db_user_access/routes/users.js
+++ 4_db_user_access/routes/users.js
@@ -1,19 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
-/* GET users listing. */
+/** Routing */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register',{ title: 'Register' });
+  res.render('register',{title:'Register'});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login',{ title: 'Login' });
+  res.render('login', {title:'Login'});
 });
 
+/** Registration page controller */
+router.post('/register', upload.single('profileimage') ,function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
 
+  if(req.file){
+  	console.log('Uploading File...');
+    console.log(req.file);
+  	var profileimage = req.file.filename;
+  } else {
+  	console.log('No File Uploaded...');
+  	var profileimage = 'noimage.jpg';
+  }
+
+  // Validation for form provided
+  req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email is not valid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+  // Check for errors
+  var errors = req.validationErrors();
+
+  // If errors are present, reload page with additional errors param
+  if(errors){
+  	res.render('register', { errors: errors });
+  } else{
+  	console.log('Form successfully submitted without errors.');
+  }
+});
 
 module.exports = router;
